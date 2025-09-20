10a1cbf5f70b9e31309458872ce24307d71b814b
Jaco Swart <jacoswart@gmail.com>
2025-09-20 21:23:30 +0100

    Change count

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ def details() :
     return jsonify({
         'time': datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
         'hostname': socket.gethostname(),
-        'message': "Server is UP!"
+        'message': "Change count #1"
     })
 
 @app.route('/api/v1/health')
